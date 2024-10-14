eba7f9ff0f197f9cd48c58570694ad229867f241
Rene Schallner <rene@renerocks.ai>
2024-10-14 11:07:56 +0200

    fixed host_url

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ const zine = @import("zine");
 pub fn build(b: *std.Build) !void {
     zine.website(b, .{
         .title = "Zine - Static Site Generator",
-        .host_url = "https://zine-ssg.io",
+        .host_url = "https://renerocks.ai",
         .layouts_dir_path = "layouts",
         .content_dir_path = "content",
         .assets_dir_path = "assets",
